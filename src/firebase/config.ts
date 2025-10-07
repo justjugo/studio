16eb91d3fb27b3035d0d75ebc8d07d4b9ac25b7c
Jugurtha Mecerrem <mecerremjugurtha@gmail.com>
2025-10-07 07:57:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4048159949-71f88",
+  "appId": "1:819851381417:web:18a704221dbd6c153c77b8",
+  "apiKey": "AIzaSyDowaLPFSGqzmGpPUie0uzKx2ZUV-JqKpo",
+  "authDomain": "studio-4048159949-71f88.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "819851381417"
+};
