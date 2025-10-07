330d9fa41bdf6976eac7490ec5e86468f56c9440
Jugurtha Mecerrem <mecerremjugurtha@gmail.com>
2025-10-07 16:23:36 +0000

    somtimes while using my phone i get this error Application error: a clie

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,26 @@
-import * as React from "react"
+import * as React from 'react';
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+    const checkDevice = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
 
-  return !!isMobile
+    // Check on mount (client-side)
+    checkDevice();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', checkDevice);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', checkDevice);
+    };
+  }, []);
+
+  return isMobile;
 }
